293d114d6855d5c46f9135e7d1edf0553f3e743e
wksmile <415893570@qq.com>
2017-12-22 09:49:50 +0800

    基本完成用户详情页面组件

--- src/components/User/index.vue
+++ src/components/User/index.vue
@@ -8,18 +8,14 @@
       </div>
       <div class="middle">
         <div class="detail">
-          <span class="nickname">{{information.nickname}}</span>
+          <span class="nickname">{{information.nickname}}</span><span v-if="information.sex"><img :src="sexPic"></span>
           <span>
-            <img src="../../public/images/address.png">
-            {{information.address}}
-          </span>
-          <span v-if="information.sex">
-            <img :src="sexPic">
+            {{information.address}} <img src="../../public/images/address.png">
           </span>
           <span v-if="information.age">
             年龄：{{information.age}}
           </span>
-          <span>
+          <span class="set">
             <router-link to="/editUser">
               编辑个人信息
               <img src="../../public/images/setting.png">
@@ -27,11 +23,21 @@
           </span>
         </div>
         <div class="user-choose">
-          <span @click="changeComponent('Attention')">关注</span>
-          <span @click="changeComponent('Fans')">粉丝</span>
-          <span @click="changeComponent('Launched')">已发布</span>
-          <span @click="changeComponent('Collect')">收藏</span>
-          <span @click="changeComponent('Collect')">赞</span>
+          <span @click="changeComponent('Launched')">已发布
+            <span>{{publichNum.contentNum}}</span>
+          </span>
+          <span @click="changeComponent('Attention')">关注
+            <span>{{publichNum.followNum}}</span>
+          </span>
+          <span @click="changeComponent('Fans')">粉丝
+            <span>{{publichNum.fanNum}}</span>
+          </span>
+          <span @click="changeComponent('Launched')">收藏
+            <span>{{publichNum.favoriteNum}}</span>
+          </span>
+          <span @click="changeComponent('Launched')">赞
+            <span>{{publichNum.thumbupNum}}</span>
+          </span>
         </div>
       </div>
       <div class="bottom">
@@ -46,6 +52,7 @@
 <script>
   import {mapState, mapMutations} from 'vuex';
   import $ from 'jquery';
+  import Cookies from 'js-cookie';
   import Attention from './Attention';   // 已关注
   import Collect from './Collect';       // 已收藏
   import Launched from './Launched';     // 已发布
@@ -57,8 +64,12 @@
   export default {
     data () {
       return {
-        view: 'Attention'
+        view: 'Launched',
+        publichNum: null      // 已发布，关注、、、个数
       }
+    },
+    beforeMount () {
+      this.isLogined();
     },
     computed: {
       ...mapState(['information']),
@@ -68,29 +79,50 @@
         return null;
       }
     },
-    created () {
-      if(Cookies.get('token')) {
-        console.log('token:---',Cookies.get('token'));
-        var that = this;
-        $.post("http://192.168.1.104:8083/user/token",{
-          token: Cookies.get('token')
-        })
-        .done(function(res){
-          if(res.status == 200 && res.data.id == that.information.id) {
-            that.SET_INFORMATION(res.data);
-          } else {
-              return;
-          }
-          console.log('getToken:',res);
-        });
-      } else {
-        console.log('has not logined');
-      }
-    },
     methods: {
-      ...mapMutations(['SET_INFORMATION']),
+      ...mapMutations(['SET_INFORMATION','SET_SHOW_LOGIN']),
+      // 判断是否登录从而请求
+      isLogined () {
+        if(Cookies.get('token')) {
+          console.log('token:---',Cookies.get('token'));
+          var that = this;
+          $.post("http://192.168.1.104:8083/user/token",{
+            token: Cookies.get('token')
+          })
+          .done(function(res){
+            if(res.status == 200) {
+              console.log('-------90909999');
+              that.SET_INFORMATION(res.data);
+              // 请求个数
+              that.reqNumbers();
+              that.setHeader();
+            } else {
+              // 应该加一个弹框提示未登录
+              return;
+            }
+            console.log('getToken:',res);
+          });
+        } else {
+          console.log('has not logined');
+        }
+      },
       changeComponent (cpt) {
         this.view = cpt;
+      },
+      setHeader () {
+        this.SET_SHOW_LOGIN(false);
+      },
+      reqNumbers () {
+        var that = this;
+        $.post("http://192.168.1.104:8081/user/info/number",{
+          userId: that.information.id
+        })
+        .done(function(res){
+          console.log('已赞个数',res);
+          if(res.status == 200) {
+            that.publichNum = res.data;
+          }
+        });
       }
     },
     components: {
@@ -140,6 +172,11 @@
     border-radius: 50%;
   }
 
+  .photo img {
+    width: 80px;
+    height: 80px;
+  }
+
   .middle {
     height: 100px;
     width: 100%;
@@ -148,6 +185,8 @@
 
   .detail {
     margin-left: 17%;
+    display: flex;
+    align-items: center;
   }
 
   .detail span {
@@ -156,8 +195,12 @@
   }
 
   .detail img {
-    width: 20px;
-    height: 20px;
+    width: 15px;
+    height: 15px;
+  }
+
+  .set a {
+    text-decoration: none;
   }
 
   .nickname {
