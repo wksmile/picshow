2cc1e4721c9fa52f2e4f29f86a26ca3cb3f7e46c
wksmile <415893570@qq.com>
2017-12-28 09:24:06 +0800

    完善

---
+++ src/components/common/SingleImg/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="content">
+    <img class="img" :src="imgSrc">
+  </div>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                imgSrc: 'http://192.168.1.104:9091/group1/M00/00/00/wKgZhVmVV_qAGKarAAHxJw8JM7M836.jpg'
+            }
+        }
+    }
+</script>
+
+<style scoped>
+  .content {
+    margin: 15px;
+  }
+
+  .img {
+    width: 25%;
+  }
+</style>
