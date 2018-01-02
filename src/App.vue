dfaa3270ce3385014369c407818c148e661c813f
wksmile <415893570@qq.com>
2018-01-02 09:11:58 +0800

    修改文件

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,43 @@
 <template>
   <div id="app">
-    <nav>
-      <div class="header-wrapepr">
-        <div class="block pic">
-          <span>
-            <img src="./public/images/instagram.png" width="35" height="35">
-          </span>
-          <span>
-            <img src="./public/images/Instagram_font.png" height="35">
-          </span>
-          <span class="login">
-            登录
-          </span>
-          <span class="reg">
-            注册
-          </span>
-        </div>
-        <div class="block search">
-          <input placeholder=" 搜 索" />
-        </div>
-        <div class="block user">
-          <span>
-            <img src="./public/images/change.png" width="25">
-          </span>
-          <span>
-            <img src="./public/images/heart_line.png" width="25">
-          </span>
-          <span>
-            <img src="./public/images/people_copy_3.png" width="25">
-          </span>
-        </div>
-      </div>
-    </nav>
+    <header-top :isShowTxt="isShowTxt"></header-top>
     <router-view/>
   </div>
 </template>
 
 <script>
+  import {mapState} from 'vuex';
+  import HeaderTop from './components/common/HeaderTop';
+
 export default {
-  name: 'app'
+  name: 'app',
+  data () {
+      return {
+      }
+  },
+  computed: {
+    ...mapState([
+      'isShowTxt'
+    ])
+  },
+  components: {
+    HeaderTop
+  }
 }
 </script>
 
 <style>
-  body {
+  @font-face {
+    font-family: 'huawen';
+    src: url('./font/huawenlishu.ttf') format('truetype')
+  }
+
+  html,body {
+    padding: 0;
     margin: 0;
-    padding: 0
+    width: 100%;
+    height: 100%;
+    background-color: #f7f8fa;
   }
 
 #app {
@@ -54,50 +46,7 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#app nav {
-  position: fixed;
-  top: 0;
-  left: 0;
   width: 100%;
-  height: 77px;
-  background-color: rgba(255,255,255,0.37);
-  border-bottom: 1px solid #ddd;
+  height: 100%;
 }
-
-#app .header-wrapepr {
-  width: 100%;
-  height: 77px;
-  display: flex;
-  align-items: center;
-}
-
-  .block {
-    display: inline-block;
-    width: 33.3%;
-  }
-
-  .block.pic span {
-    display: inline-block;
-    padding: 0 10px;
-  }
-
-  .block.pic span:first-child {
-    border-right: 1px solid #999;
-  }
-
-  .block.search input {
-    border: 1px solid #ddd;
-    height: 30px;
-    border-radius: 3px;
-  }
-
-  .block.user span {
-    margin-right: 20px;
-  }
-
-  .login, .reg {
-    float: right;
-  }
 </style>
